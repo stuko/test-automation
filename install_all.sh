73526f86472033e78c07af81f3995738a7a4c1da
stuko <stuko@nate.com>
2022-04-27 16:29:45 +0900

    modify install shell

--- install_all.sh
+++ install_all.sh
@@ -1,9 +1,12 @@
 cd ./apache-jmeter-5.4.1/bin
-./jmeter-server -Djava.rmi.server.hostname=192.168.57.224 &
+pkill -9 -ef jmeter
+./jmeter-server -Djava.rmi.server.hostname=192.168.57.224 > jmeter-server.out &
 cd ../../
 cd ./kanboard/
-sudo docker-compose -d -f docker-compose-with-mariadb up
+sudo docker-compose -f docker-compose-with-mariadb.yml down
+sudo docker-compose -f docker-compose-with-mariadb.yml up -d
 cd ../
+sudo docker container stop --name mongo-test-automation
 sudo docker run -d --name mongo-test-automation -e "TZ=Asia/Seoul" -p 27001:27001 --mount type=bind,source=${PWD}/volume/mongo/data,target=/data/db -d  --restart always mongo:4.4.10
 cd ./test-controller/
 ./docker-run.sh
--- test-controller/docker-run.sh
+++ test-controller/docker-run.sh
@@ -1,1 +1,2 @@
+sudo docker container stop --name test-controller-python
 sudo docker run -d -p 5000:5000 --name test-controller-python test-controller-python
