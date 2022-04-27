cdb885540f36710206d0963c2034c1f28ba08068
stuko <stuko@nate.com>
2022-04-27 16:13:57 +0900

    add install_all

--- install_all.sh
+++ install_all.sh
@@ -2,5 +2,12 @@ cd ./apache-jmeter-5.4.1/bin
 ./jmeter-server -Djava.rmi.server.hostname=192.168.57.224 &
 cd ../../
 cd ./kanboard/
-docker-compose -d -f docker-compose-with-mariadb up
-docker run -d --name mongo-test-automation -e "TZ=Asia/Seoul" -p 27001:27001 --mount type=bind,source=${PWD}/volume/mongo/data,target=/data/db -d  --restart always mongo:4.4.10
+sudo docker-compose -d -f docker-compose-with-mariadb up
+cd ../
+sudo docker run -d --name mongo-test-automation -e "TZ=Asia/Seoul" -p 27001:27001 --mount type=bind,source=${PWD}/volume/mongo/data,target=/data/db -d  --restart always mongo:4.4.10
+cd ./test-controller/
+./docker-run.sh
+cd ../
+
+
+
