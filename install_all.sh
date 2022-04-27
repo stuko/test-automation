0d5f626004be653416a7737abaf5142b3c46028e
stuko <stuko@nate.com>
2022-04-27 16:39:20 +0900

    remove container

--- install_all.sh
+++ install_all.sh
@@ -8,6 +8,7 @@ sudo docker-compose -f docker-compose-with-mariadb.yml down
 sudo docker-compose -f docker-compose-with-mariadb.yml up -d
 cd ../
 sudo docker container stop mongo-test-automation
+sudo docker container rm mongo-test-automation
 sudo docker run -d --name mongo-test-automation -e "TZ=Asia/Seoul" -p 27001:27001 --mount type=bind,source=${PWD}/volume/mongo/data,target=/data/db -d  --restart always mongo:4.4.10
 cd ./test-controller/
 chmod a+x *.sh
--- test-controller/docker-run.sh
+++ test-controller/docker-run.sh
@@ -1,2 +1,3 @@
 sudo docker container stop test-controller-python
+sudo docker container rm test-controller-python
 sudo docker run -d -p 5000:5000 --name test-controller-python test-controller-python
