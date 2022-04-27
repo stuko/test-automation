348302889a73369bded34508c01446d683ed7b35
stuko <stuko@nate.com>
2022-04-27 16:36:31 +0900

    docker stop

--- test-controller/docker-run.sh
+++ test-controller/docker-run.sh
@@ -1,2 +1,2 @@
-sudo docker container stop --name test-controller-python
+sudo docker container stop test-controller-python
 sudo docker run -d -p 5000:5000 --name test-controller-python test-controller-python
