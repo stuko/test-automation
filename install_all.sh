55da9fe2576f04d1df22960d5824d739206a4185
stuko <stuko@nate.com>
2022-04-27 16:32:42 +0900

    modify shell permission

--- install_all.sh
+++ install_all.sh
@@ -1,4 +1,5 @@
 cd ./apache-jmeter-5.4.1/bin
+chmod a+x *.sh jmeter*
 pkill -9 -ef jmeter
 ./jmeter-server -Djava.rmi.server.hostname=192.168.57.224 > jmeter-server.out &
 cd ../../
@@ -9,6 +10,7 @@ cd ../
 sudo docker container stop --name mongo-test-automation
 sudo docker run -d --name mongo-test-automation -e "TZ=Asia/Seoul" -p 27001:27001 --mount type=bind,source=${PWD}/volume/mongo/data,target=/data/db -d  --restart always mongo:4.4.10
 cd ./test-controller/
+chmod a+x *.sh
 ./docker-run.sh
 cd ../
 
