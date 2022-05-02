2433b734c96c42acf163b439901ea7bcce33bc73
stuko <stuko@nate.com>
2022-05-02 11:59:34 +0900

    add env var

--- test-controller/docker-run.sh
+++ test-controller/docker-run.sh
@@ -1,3 +1,3 @@
 sudo docker container stop test-controller-python
 sudo docker container rm test-controller-python
-sudo docker run -d -p 5000:5000 --name test-controller-python test-controller-python
+sudo docker run -d -p 5000:5000 -e MONGO_IP=${MONGO_IP} -e MONGO_PORT={MONGO_PORT} --name test-controller-python test-controller-python
