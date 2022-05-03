96d211ea45a639d57031c1a027e6eedb871d4644
stuko <stuko@nate.com>
2022-05-03 16:41:38 +0900

    add debug

--- test-controller/docker-build.sh
+++ test-controller/docker-build.sh
@@ -1,3 +1,3 @@
 sudo docker image rm test-controller-python
-sudo docker build -t test-controller-python .
+sudo docker build -t self/test-controller-python:lates .
 # sudo docker run -d -p 5000:5000 --name test-controller-python test-controller-python
