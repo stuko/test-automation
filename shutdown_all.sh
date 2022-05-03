63fa5f88678306b690d3609e9ee1ed19a034550f
stuko <stuko@nate.com>
2022-05-03 09:11:44 +0900

    add shutdown

---
+++ shutdown_all.sh
@@ -0,0 +1,15 @@
+cd ./apache-jmeter-5.4.1/bin
+pkill -9 -ef jmeter
+cd ../../
+cd ./kanboard/
+sudo docker-compose -f docker-compose-with-mariadb.yml down
+cd ../
+sudo docker container stop mongo-test-automation
+sudo docker container rm mongo-test-automation
+cd ./test-controller/
+sudo docker container stop test-controller-python
+sudo docker container rm test-controller-python
+cd ../
+
+
+
