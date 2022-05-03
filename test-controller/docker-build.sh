65eaa60ea6b01fccd0e0163c6f995f4ba97ef31d
stuko <stuko@nate.com>
2022-05-03 10:09:11 +0900

    add docker rm shell

--- test-controller/docker-build.sh
+++ test-controller/docker-build.sh
@@ -1,2 +1,3 @@
+sudo docker image rm test/test-controller-python
 sudo docker build -t test/test-controller-python:latest .
 # sudo docker run -d -p 5000:5000 --name test-controller-python test-controller-python
