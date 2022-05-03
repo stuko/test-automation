6e3a53619bcc774829ae28a3fb42415994e56d43
stuko <stuko@nate.com>
2022-05-03 09:04:46 +0900

    add docker time zone

--- test-controller/docker-run.sh
+++ test-controller/docker-run.sh
@@ -1,3 +1,3 @@
 sudo docker container stop test-controller-python
 sudo docker container rm test-controller-python
-sudo docker run -d -p 5000:5000 --mount type=bind,target=/app/server/volume/upload,source="$(pwd)"/../volume/upload --mount type=bind,target=/app/server/volume/config,source="$(pwd)"/../volume/config -e MONGO_IP=${MONGO_IP} -e MONGO_PORT={MONGO_PORT} --name test-controller-python test-controller-python
+sudo docker run -d -p 5000:5000 --mount type=bind,target=/app/server/volume/upload,source="$(pwd)"/../volume/upload --mount type=bind,target=/app/server/volume/config,source="$(pwd)"/../volume/config -e MONGO_IP=${MONGO_IP} -e MONGO_PORT={MONGO_PORT} -e "TZ=Asia/Seoul" --name test-controller-python test-controller-python
