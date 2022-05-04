abeb8d9956ca27bbc5720940befdeca92231f24d
stuko <stuko@nate.com>
2022-05-04 11:36:06 +0900

    modify controller of config

--- install_all.sh
+++ install_all.sh
@@ -1,9 +1,10 @@
 sudo docker container prune
 sudo docker rm -f test-controller-python
+export MY_LOCAL_IP=`hostname -I | cut -d' ' -f1`
 cd ./apache-jmeter-5.4.1/bin
 chmod a+x *.sh jmeter*
 pkill -9 -ef jmeter
-./jmeter-server -Djava.rmi.server.hostname=192.168.57.224 > jmeter-server.out &
+./jmeter-server -Djava.rmi.server.hostname=${MY_LOCAL_IP} > jmeter-server.out &
 cd ../../
 cd ./kanboard/
 sudo docker-compose -f docker-compose-with-mariadb.yml down
@@ -13,10 +14,10 @@ sudo docker container stop mongo-test-automation
 sudo docker container rm mongo-test-automation
 # sudo docker run -d --name mongo-test-automation -e "TZ=Asia/Seoul" -p 27017:27017 --mount type=bind,source=${PWD}/mongodb/init,target=/docker-entrypoint-initdb.d --mount type=bind,source=${PWD}/volume/mongo/data,target=/data/db -d  --restart always mongo:4.4.10
 sudo docker run -d --name mongo-test-automation -e "TZ=Asia/Seoul" -p 27017:27017 --mount type=bind,source=${PWD}/volume/mongo/data,target=/data/db -d  --restart always mongo:4.4.10
-cd ./mongodb/init/
-chmod a+x *.sh
-./init.sh
-cd ../..
+# cd ./mongodb/init/
+# chmod a+x *.sh
+# ./init.sh
+# cd ../..
 cd ./test-controller/
 chmod a+x *.sh
 ./docker-build.sh
