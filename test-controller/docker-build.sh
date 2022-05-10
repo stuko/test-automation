98a12dd6ca0e1ddf71f451debc59f74f65ea418f
stuko <stuko@nate.com>
2022-05-10 11:41:35 +0900

    add jdk to docker

--- test-controller/docker-build.sh
+++ test-controller/docker-build.sh
@@ -1,3 +1,4 @@
+chmod a+x ./apache-jmeter-5.4.1/bin/*.sh ./apache-jmeter-5.4.1/bin/jmeter*
 sudo docker image rm test-controller-python
 sudo docker build -t test-controller-python .
 #sudo docker build -t self/test-controller-python:lates .
