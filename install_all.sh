d339ee34d11f81ce1ff6041d9afd83555c180f49
stuko <stuko@nate.com>
2022-05-13 09:10:50 +0900

    add getting started

--- install_all.sh
+++ install_all.sh
@@ -1,4 +1,9 @@
 sudo docker container prune
+cd ./kanboard/
+sudo docker-compose -f docker-compose-with-mariadb.yml down
+sudo docker-compose -f docker-compose-with-mariadb.yml up -d
+cd ../
+
 sudo docker rm -f test-controller-python
 export MY_LOCAL_IP=`hostname -I | cut -d' ' -f1`
 cd ./apache-jmeter-5.4.1/bin
@@ -7,10 +12,6 @@ pkill -9 -ef jmeter
 ./jmeter-server -DTEST_AUTO=true -Djava.rmi.server.hostname=${MY_LOCAL_IP} > jmeter-server.out &
 cd ../../
 java -cp .:./apache-jmeter-5.4.1/lib/ext/test-automation-jmeter-plugin.jar com.auto.test.jmeter.plugin.common.server.ShellServer &
-cd ./kanboard/
-sudo docker-compose -f docker-compose-with-mariadb.yml down
-sudo docker-compose -f docker-compose-with-mariadb.yml up -d
-cd ../
 sudo docker container stop mongo-test-automation
 sudo docker container rm mongo-test-automation
 # sudo docker run -d --name mongo-test-automation -e "TZ=Asia/Seoul" -p 27017:27017 --mount type=bind,source=${PWD}/mongodb/init,target=/docker-entrypoint-initdb.d --mount type=bind,source=${PWD}/volume/mongo/data,target=/data/db -d  --restart always mongo:4.4.10
