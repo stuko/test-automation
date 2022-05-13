12538841187806398726ba6a4a4684a61fef0207
stuko <stuko@nate.com>
2022-05-13 10:18:08 +0900

    modify kill

--- test-controller/test-controller.py
+++ test-controller/test-controller.py
@@ -118,9 +118,10 @@ def controller():
             
             nowDatetime = datetime.datetime.now()
             os.makedirs(result_folder, exist_ok=True)
-            result_file_name = result_folder + "result-" + project_id + "-" + nowDatetime.strftime("%Y%m%d%H%M%S")
+            result_name = "result-" + project_id + "-" + nowDatetime.strftime("%Y%m%d%H%M%S")
+            result_file_name = result_folder + result_name
             jm.execute_shell_command(jm.get_shell_command(upload_folder + jmx_file_name , result_file_name))
-            result_string = open("/app/server/" + result_file_name, 'r').read()
+            result_string = open("/app/server/volume/result/" + result_name, 'r').read()
 
             mm.send_test_automation_complete(project_id,project_name, task_title,result_string)              
             km.move(project_id, task_id, swimlane_id, forward, position)
