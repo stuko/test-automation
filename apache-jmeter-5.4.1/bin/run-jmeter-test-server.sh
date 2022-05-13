37e507a59f39f25ebc38f65f63bf92f3393a60d3
stuko <stuko@nate.com>
2022-05-13 14:35:16 +0900

    modify jmeter shell

--- apache-jmeter-5.4.1/bin/run-jmeter-test-server.sh
+++ apache-jmeter-5.4.1/bin/run-jmeter-test-server.sh
@@ -1,3 +1,4 @@
+export MY_LOCAL_IP=`hostname -I | cut -d' ' -f1`
 ./jmeter-server -DTEST_AUTO=true -Djava.rmi.server.hostname=${MY_LOCAL_IP} > jmeter-server.out &
 pkill -9 -ef com.auto.test.jmeter.plugin.common.server.ShellServer
 java -cp .:../lib/ext/test-automation-jmeter-plugin.jar com.auto.test.jmeter.plugin.common.server.ShellServer &
