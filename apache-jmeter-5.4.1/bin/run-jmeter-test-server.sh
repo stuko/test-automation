3f4879605dc547d5a2498ba7b60c63fce3dc01be
stuko <stuko@nate.com>
2022-05-18 13:29:20 +0900

    jmeter shell error

--- apache-jmeter-5.4.1/bin/run-jmeter-test-server.sh
+++ apache-jmeter-5.4.1/bin/run-jmeter-test-server.sh
@@ -1,4 +1,8 @@
 export MY_LOCAL_IP=`hostname -I | cut -d' ' -f1`
-./jmeter-server -DTEST_AUTO=true -Djava.rmi.server.hostname=${MY_LOCAL_IP} > jmeter-server.out &
-pkill -9 -ef com.auto.test.jmeter.plugin.common.server.ShellServer
-java -cp .:../lib/ext/test-automation-jmeter-plugin.jar com.auto.test.jmeter.plugin.common.server.ShellServer &
+sudo pkill -9 -ef jmeter
+sleep 1
+sudo chmod a+x *.sh jmeter*
+sudo nohup ./jmeter-server -DTEST_AUTO=true -Djava.rmi.server.hostname=${MY_LOCAL_IP} > jmeter-server.out &
+sudo pkill -9 -ef com.auto.test.jmeter.plugin.common.server.ShellServer
+sleep 1
+sudo nohup java -cp .:../lib/ext/test-automation-jmeter-plugin.jar com.auto.test.jmeter.plugin.common.server.ShellServer &
