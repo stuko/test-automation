fc3ea7413393b348eaa2d2a404d1f3bca7b19a9d
stuko <stuko@nate.com>
2022-05-19 11:12:15 +0900

    add test debugging of restart shell

--- apache-jmeter-5.4.1/bin/run-jmeter-test-server.sh
+++ apache-jmeter-5.4.1/bin/run-jmeter-test-server.sh
@@ -1,5 +1,5 @@
 export MY_LOCAL_IP=`hostname -I | cut -d' ' -f1`
-sudo pkill -9 -ef jmeter
+sudo pkill -9 -ef jmeter-server
 sleep 1
 sudo chmod a+x *.sh jmeter*
 sudo nohup ./jmeter-server -DTEST_AUTO=true -Djava.rmi.server.hostname=${MY_LOCAL_IP} > jmeter-server.out &
