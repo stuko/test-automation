7dbfb9b8a4f63b53023b53e384afde4b44f45389
stuko <stuko@nate.com>
2022-05-27 10:51:49 +0900

    execute_shell_command argument error

--- test-controller/jmeter_manager.py
+++ test-controller/jmeter_manager.py
@@ -93,7 +93,7 @@ class JmeterManager:
     def stop_test(self):
         # self.execute_shell_command(self, self.path + 'shutdown.sh')
         # self.execute_shell_command(self, self.path + 'stoptest.sh')
-        self.execute_shell_command(self, 'pkill -9 -ef EXEC_JMETER_TEST', True)
+        self.execute_shell_command('pkill -9 -ef EXEC_JMETER_TEST', True)
 
     def execute_shell_command(self, shell , is_root=True):
         if is_root : 
